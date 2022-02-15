0de693eeaf8fece7f74552e01e4959175b1a4e39
HabibIbrahimov <habibyi@code.edu.az>
2022-02-15 11:38:41 +0400

    feat:product CRUD

---
+++ First_Api/Data/DAL/Context.cs
@@ -0,0 +1,18 @@
+﻿using First_Api.Data.Entity;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace First_Api.Data.DAL
+{
+    public class Context:DbContext
+    {
+        public Context(DbContextOptions<Context>options):base(options)
+        {
+
+        }
+        public DbSet<Product> Products { get; set; }
+    }
+}
